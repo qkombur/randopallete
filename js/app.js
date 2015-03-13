318598384b639e778945166cab2fe757a09f2197
qkombur <chinofiron@gmail.com>
2015-03-12 21:39:28 -0400

    Comments
    Added comments to javascript

--- js/app.js
+++ js/app.js
@@ -6,11 +6,11 @@ var rgbColor;
 var hexCode;
 
 
-
+// used to reload page on button press
 function newPalette() {
     location.reload();
 }
-
+// translates rgb values into hexcode 
 function componentToHex(c) {
     var hex = c.toString(16);
     return hex.length == 1 ? "0" + hex : hex;
@@ -20,11 +20,13 @@ function rgbToHex(r, g, b) {
     return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
 }
 
+// loop that generates rgb values and outputs a div class to the page.
 for ( var i = 0; i < 9; i+=1) {
 	html = '<div class="color"' + '>' +
 	'</div>';
 	document.write(html);
 	var elems = document.getElementsByClassName('color');
+	//this loop is used to avoid generating inline styles
 		for(var i = 0; i < elems.length; i++) {
 			r = Math.floor(Math.random() * 256 );
 			g = Math.floor(Math.random() * 256 );
